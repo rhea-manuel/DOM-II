d0742134888475b193b9ff99e43b4b424b271549
rhea-manuel <rheamanuel02@gmail.com>
2020-09-01 16:16:13 -0400

    Added 2 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+
+// ------------------
+// First modifying the nav links
+// ------------------
+const allNavLinks = document.querySelectorAll(".nav-link")
+// allNavLinks.forEach(
+//     item=>item.addEventListener('mouseover', function(event){
+//     item.style.color="green"
+// }
+// item.addEventListener('mouseleave', function(event){
+    
+// })
+// ))
+
+allNavLinks.forEach(function(item){
+
+    // ------------------
+    // 1st type: Mouseover
+    // ------------------
+    item.addEventListener("mouseover", function(event){
+        item.style.color="#17a2b8"
+        item.style.transition=".25s"
+    })
+
+    // ------------------
+    // 2nd type: Moveleave
+    // ------------------
+    item.addEventListener("mouseleave", function(event){
+        item.style.color="#213248"
+    })
+})
+
+
